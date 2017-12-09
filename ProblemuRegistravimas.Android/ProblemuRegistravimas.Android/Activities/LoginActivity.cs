98f30e0731b3b467cc7190b9870606dde4b18bdd
tomval <tomovaliuno@gmail.com>
2017-12-09 19:09:51 +0200

    Initial commit with login page

---
+++ ProblemuRegistravimas.Android/ProblemuRegistravimas.Android/Activities/LoginActivity.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ProblemuRegistravimas.Android.Activities
+{
+    [Activity(Label = "LoginActivity", MainLauncher = true)]
+    public class LoginActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.Login);
+            var loginButton = FindViewById<Button>(Resource.Id.loginButton);
+            loginButton.Click += LoginButton_Click;
+
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
